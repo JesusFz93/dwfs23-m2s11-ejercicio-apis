f165894a3a2bae717354238e4f708b9378df8d50
Jesus <jesus.fdz93@gmail.com>
2023-11-25 12:48:14 -0600

    metodos http

--- js/4-metodos_http.js
+++ js/4-metodos_http.js
@@ -9,3 +9,59 @@ const obtenerProductos = async () => {
     console.log(error);
   }
 };
+
+const agregarProducto = async () => {
+  try {
+    const resp = await fetch(URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title: "test product",
+        price: 13.5,
+        description: "lorem ipsum set",
+        image: "https://i.pravatar.cc",
+        category: "electronic",
+      }),
+    });
+    const json = await resp.json();
+    console.log(json);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const actualizarProducto = async () => {
+  try {
+    const resp = await fetch(`${URL}/7`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title: "test product",
+        price: 13.5,
+        description: "lorem ipsum set",
+        image: "https://i.pravatar.cc",
+        category: "electronic",
+      }),
+    });
+    const json = await resp.json();
+    console.log(json);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const eliminarProducto = async () => {
+  try {
+    const resp = await fetch(`${URL}/6`, {
+      method: "DELETE",
+    });
+    const json = await resp.json();
+    console.log(json);
+  } catch (error) {
+    console.log(error);
+  }
+};
